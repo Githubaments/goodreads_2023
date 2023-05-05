24cc796405c6f72f09273659821b92e564664176
Githubaments <flynnshane@gmail.com>
2023-05-05 22:34:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-# ... (rest of the imports and functions)
+import streamlit as st
+import pandas as pd
+import numpy as np
+import gender_guesser.detector as gender
+import plotly.express as px
+
+# Load data
+def load_data(file):
+    data = pd.read_csv(file, parse_dates=['Date Read', 'Date Added'])
+    return data
+
+def get_gender(name):
+    d = gender.Detector()
+    first_name = name.split()[0]
+    return d.get_gender(first_name)
 
 st.title('Goodreads Book Analysis')
 
@@ -6,7 +20,24 @@ uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     data = load_data(uploaded_file)
 
-    # ... (rest of the code)
+    st.write('### Cumulative Books Read Over Time')
+    books_read = data['Date Read'].value_counts().resample('M').sum().dropna()
+    cumulative_books_read = books_read.cumsum()
+    fig = px.line(x=cumulative_books_read.index, y=cumulative_books_read, labels={'x': 'Date', 'y': 'Cumulative Books Read'})
+    st.plotly_chart(fig)
+
+    st.write('### Top Authors')
+    top_authors = data['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+    st.write('### Average Rating by Year')
+    data['Year Read'] = data['Date Read'].dt.year
+    avg_ratings_by_year = data.groupby('Year Read')['My Rating'].mean().dropna()
+    st.line_chart(avg_ratings_by_year)
+
+    st.write('### Distribution of Ratings')
+    ratings_count = data['My Rating'].value_counts().sort_index()
+    st.bar_chart(ratings_count)
 
     st.write('### Scatter Plot: Length vs. Average Rating')
     fig = px.scatter(data, x='Number of Pages', y='Average Rating', hover_name='Title', labels={'x': 'Number of Pages', 'y': 'Average Rating'})
